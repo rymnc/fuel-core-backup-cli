437039e11a81137d8c2500fa69c3148f7d9f2b76
rymnc <43716372+rymnc@users.noreply.github.com>
2025-01-08 07:52:04 +0530

    fix: temp dir handling

--- src/backup.rs
+++ src/backup.rs
@@ -12,14 +12,14 @@ pub fn backup(db_dir: &str, backup_path: &str) -> anyhow::Result<()> {
 #[cfg(feature = "compress")]
 pub fn backup(db_dir: &str, backup_path: &str) -> anyhow::Result<()> {
     use crate::compression::compress_directory;
+    use tempfile::TempDir;
 
-    let tmp_backup_dir = std::env::temp_dir().join("fuel-db-backup");
+    let tmp_backup_dir = TempDir::new()?;
     let db_dir = std::path::Path::new(db_dir);
     let backup_file = std::path::Path::new(backup_path);
 
-    CombinedDatabase::backup(db_dir, &tmp_backup_dir)?;
-    compress_directory(&tmp_backup_dir, backup_file)?;
-    std::fs::remove_dir_all(&tmp_backup_dir)?;
+    CombinedDatabase::backup(db_dir, &tmp_backup_dir.path())?;
+    compress_directory(&tmp_backup_dir.path(), backup_file)?;
 
     Ok(())
 }
--- src/restore.rs
+++ src/restore.rs
@@ -12,14 +12,14 @@ pub fn restore(restore_to: &str, backup_path: &str) -> anyhow::Result<()> {
 #[cfg(feature = "compress")]
 pub fn restore(restore_to: &str, backup_path: &str) -> anyhow::Result<()> {
     use crate::compression::decompress_archive;
+    use tempfile::TempDir;
 
     let backup_path = std::path::Path::new(backup_path);
     let restore_to = std::path::Path::new(restore_to);
-    let tmp_backup_dir = std::env::temp_dir().join("fuel-db-restore");
+    let tmp_backup_dir = TempDir::new()?;
 
-    decompress_archive(backup_path, &tmp_backup_dir)?;
-    CombinedDatabase::restore(restore_to, &tmp_backup_dir)?;
-    std::fs::remove_dir_all(&tmp_backup_dir)?;
+    decompress_archive(backup_path, &tmp_backup_dir.path())?;
+    CombinedDatabase::restore(restore_to, &tmp_backup_dir.path())?;
 
     Ok(())
 }
